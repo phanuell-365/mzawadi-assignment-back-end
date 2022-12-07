a76d24f5b3c768e9330bb765ae034c4d2c66a7b8
phanuell-365 <phanuelkidakwa@gmail.com>
2022-12-07 23:06:00 +0300

    Removed the key generator file from the .gitignore

---
+++ src/auth/keys/keys.services.ts
@@ -0,0 +1,34 @@
+import * as crypto from 'crypto';
+
+// import * as fs from 'fs';
+
+class KeysService {
+  static createKeyPair() {
+    const key = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem',
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+      },
+    });
+
+    const publicKey = key.publicKey;
+    const privateKey = key.privateKey;
+
+    const publicKeyBase64 = Buffer.from(publicKey).toString('base64');
+    const privateKeyBase64 = Buffer.from(privateKey).toString('base64');
+
+    console.log(`PUP_KEY -> ${publicKeyBase64} \n\n\n\n`);
+    console.log(`PRV_KEY -> ${privateKeyBase64}`);
+    // eslint-disable-next-line no-undef
+    // fs.writeFileSync(__dirname + '/public.pem', publicKey);
+    // eslint-disable-next-line no-undef
+    // fs.writeFileSync(__dirname + '/private.pem', privateKey);
+  }
+}
+
+KeysService.createKeyPair();
